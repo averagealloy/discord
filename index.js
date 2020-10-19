fbbae2ea8fdb58ac3fa3cddde4307c0319ef5779
averagealloy <m.mctighe10@gmail.com>
2020-10-19 16:12:36 -0400

    added an args modifyer and it working lets goooo

--- index.js
+++ index.js
@@ -45,6 +45,10 @@ client.on('message', message => {
 		if (!args.length) {
 			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 		}
+		else if (args[0] === 'foo') {
+			return message.channel.send('bar');
+		}
+
 
 		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 	}
